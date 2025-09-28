91a95eba5adc8858aa0c5ff058178aa6d0a4a86c
aqxua <88640593+wifcat@users.noreply.github.com>
2025-09-28 19:57:36 +0700

    Update BullyAE_PNG2DDS.py
    Bug fix where it doesn't convert to dds

--- Bully AE Texture Converter/AE/plugins/BullyAE_PNG2DDS.py
+++ Bully AE Texture Converter/AE/plugins/BullyAE_PNG2DDS.py
@@ -1,26 +1,29 @@
+# this is using the quicktex
+
 from PIL import Image
 import subprocess, os, sys
 
-def png2dds(input_file, format="bc3"):
-    im = Image.open(input_file).convert("RGBA")
+def png2dds(input_file, format="bc1"):
+    # flip PNG
+    im = Image.open(input_file)
     im = im.transpose(Image.FLIP_TOP_BOTTOM)
-    temp_file = os.path.splitext(input_file)[0] + "_tmp.png"
+    temp_file = "temp_" + os.path.basename(input_file)
     im.save(temp_file)
 
 
     output_file = os.path.splitext(input_file)[0] + ".dds"
-
     subprocess.run([
-        "quicktex", "encode", format, temp_file, "-o", output_file
+        "quicktex", "encode", format, temp_file,
     ], check=True)
 
     os.remove(temp_file)
-    print(f"{input_file} > {output_file} ({format}) [successfully converted]")
+    print(f"{input_file} > {output_file} ({format}) [succesfully converted]")
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python BullyAE_PNG2DDS.py <file.png|jpg> [bc1|bc2|bc3|bc4|bc5]")
+    if len(sys.argv)<2:
+        print("Usage: python BullyAE_PNG2DDS.py <file.png> [bc1|bc2|bc3|bc4|bc5]")
     else:
         input_file = sys.argv[1]
-        format = sys.argv[2] if len(sys.argv) > 2 else "bc3"
+        format = sys.argv[2] if len(sys.argv)>2 else "bc1"
         png2dds(input_file, format)
+        
