5c6e4f47b056ecf78410434b1adff43063458014
aqxua <88640593+wifcat@users.noreply.github.com>
2026-02-24 08:10:58 +0700

    Update Block.lua

--- Bully Lua/Block.lua
+++ Bully Lua/Block.lua
@@ -1,17 +1,6 @@
--- Replace Blocking
-TestBlock= function()
-	while true do
-		Wait(0)
-		local gPlayer_Target = PedGetTargetPed(gPlayer)
-		if PedIsPlaying(gPlayer, "/Global/HitTree/Standing/Melee", true) 
-			and PedIsValid(gPlayer_Target) and PedIsInCombat(gPlayer_Target) 
-			and not PedIsDead(gPlayer_Target) and not PedMePlaying(gPlayer, "Front_Float") and not PedMePlaying(gPlayer, "On_Ground") 
-			and not PedMePlaying(gPlayer, "OnGroundBounce") and not PedMePlaying(gPlayer, "DownOnGround") 
-			and not PedMePlaying(gPlayer, "GroundAndWallHits") and not PedMePlaying(gPlayer, "KOReactions") and not PedMePlaying(gPlayer, "PlayerOnGround") 
-			and not PedMePlaying(gPlayer, "BellyUp") and not PedMePlaying(gPlayer, "BellyDown")  then
-			-- Derby Block 
-			PedSetActionNode(gPlayer, "/Global/BOSS_Darby/Defense/Block/Block/BlockHits/HitsLight", "act/anim/BOSS_Darby.act")
+for i = 1, 500 do
+		    if PedIsHit(gPlayer, 2) and PedIsInCombat(PedGetTargetPed(gPlayer)) then
+		        PedSetActionNode(gPlayer, "/Global/Actions/Defence/Block/Block/BlockHits/HitsLight", "Globals/GlobalActions.act")
+		        break
+		    end
 		end
-	    Wait(0)
-	end
-end
